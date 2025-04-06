a7f2ca9a122207da8ceb5d2215d1384af3654bf7
FirstName LastName <s.seade@uniandes.edu.co>
2025-04-06 10:54:44 -0500

    Taller 2 TypeScript

--- main.ts
+++ main.ts
@@ -7,18 +7,31 @@ mostrarSeriesEnTabla(series);
 
 calcularPromedioSeasons(series);
 
-function mostrarSeriesEnTabla(sers: Serie[]): void{
+document.addEventListener('click', (e) => {
+    const target = e.target as HTMLElement;
+    if(target.matches('p.clickable')) {
+      const seriesName = target.textContent;
+      const serieSeleccionada = series.filter(c => c.name === seriesName)[0];
+      
+      if(serieSeleccionada) {
+        mostrarDetalleSerie(serieSeleccionada);
+      }
+    }
+  });
+  
+
+function mostrarSeriesEnTabla(sers: Serie[]): void {
     sers.forEach(c => {
         let trElement = document.createElement("tr");
-        trElement.style.backgroundColor = "lightgresy";
-        trElement.innerHTML = `<td><b>${c.numero}</b></td>
-                           <td><a href="${c.link}">${c.name}</a></td>
-                           <td>${c.channel}</td>
-                           <td>${c.seasons}</td>`;
+        trElement.style.backgroundColor = "lightgrey";
+        trElement.innerHTML = `
+            <td><b>${c.numero}</b></td>
+            <td><p class="clickable" style="color: blue; cursor: pointer;">${c.name}</p></td>
+            <td>${c.channel}</td>
+            <td>${c.seasons}</td>`;
         seriesTbody.appendChild(trElement);
     });
 }
-
 function calcularPromedioSeasons(sers: Serie[]): void{
     let numSeries = 0;
     let totalSeries = 0;
@@ -31,3 +44,18 @@ function calcularPromedioSeasons(sers: Serie[]): void{
     trElement.innerHTML = `<td colspan="4">Seasons average: ${promedioSeasons}</td>`;    
     seriesTbody.appendChild(trElement);
 }
+
+function mostrarDetalleSerie(serie: Serie): void{
+    const cardBody = document.getElementById('card-body');
+    const card = document.getElementById('card')
+    if(cardBody) {
+        cardBody.innerHTML = `
+            <img src="${serie.imagen}" class="img-fluid" alt="${serie.imagen}">
+            <div>
+                <h5>${serie.name}</h5>
+                <p>${serie.descripcion}</p>
+                <a href="${serie.link}">${serie.link}</a>
+            </div>
+        `;
+    }
+  }
--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,21 @@ import { series } from "./data.js";
 var seriesTbody = document.getElementById('series');
 mostrarSeriesEnTabla(series);
 calcularPromedioSeasons(series);
+document.addEventListener('click', function (e) {
+    var target = e.target;
+    if (target.matches('p.clickable')) {
+        var seriesName_1 = target.textContent;
+        var serieSeleccionada = series.filter(function (c) { return c.name === seriesName_1; })[0];
+        if (serieSeleccionada) {
+            mostrarDetalleSerie(serieSeleccionada);
+        }
+    }
+});
 function mostrarSeriesEnTabla(sers) {
     sers.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.style.backgroundColor = "lightgresy";
-        trElement.innerHTML = "<td><b>".concat(c.numero, "</b></td>\n                           <td><a href=\"").concat(c.link, "\">").concat(c.name, "</a></td>\n                           <td>").concat(c.channel, "</td>\n                           <td>").concat(c.seasons, "</td>");
+        trElement.style.backgroundColor = "lightgrey";
+        trElement.innerHTML = "\n            <td><b>".concat(c.numero, "</b></td>\n            <td><p class=\"clickable\" style=\"color: blue; cursor: pointer;\">").concat(c.name, "</p></td>\n            <td>").concat(c.channel, "</td>\n            <td>").concat(c.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
 }
@@ -21,4 +31,11 @@ function calcularPromedioSeasons(sers) {
     var trElement = document.createElement("tr");
     trElement.innerHTML = "<td colspan=\"4\">Seasons average: ".concat(promedioSeasons, "</td>");
     seriesTbody.appendChild(trElement);
+}
+function mostrarDetalleSerie(serie) {
+    var cardBody = document.getElementById('card-body');
+    var card = document.getElementById('card');
+    if (cardBody) {
+        cardBody.innerHTML = "\n            <img src=\"".concat(serie.imagen, "\" class=\"img-fluid\" alt=\"").concat(serie.imagen, "\">\n            <div>\n                <h5>").concat(serie.name, "</h5>\n                <p>").concat(serie.descripcion, "</p>\n                <a href=\"").concat(serie.link, "\">").concat(serie.link, "</a>\n            </div>\n        ");
+    }
 }
